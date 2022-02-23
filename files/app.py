a169fc0fe80c671d8a76fd53ce0f54725d0ef2d1
Arda <ardayildirim05@icloud.com>
2022-02-23 21:56:21 +0100

    Version 0.2.1
    Added a new file to contain messages from the game to the player
    
    Added a feature that cleans previous messages from the command line
    
    Commenced with the start game feature

--- files/app.py
+++ files/app.py
@@ -1,8 +1,11 @@
 from data import *
 from math import *
 from random import *
+from os import *
 
-data["general"]["currentState"] = "startScreen"
+dataObject = data()
+
+dataObject["general"]["currentState"] = "startScreen"
 
 
 def playerInput(msg, validation, value):
@@ -10,10 +13,10 @@ def playerInput(msg, validation, value):
         insert = input(msg + ": ")
         shouldContinue = False
 
-        if (insert == data["settings"]["StopGame"]["CurrentSetting"]):
+        if (insert == dataObject["settings"]["StopGame"]["CurrentSetting"]):
             exit()
 
-        if (insert == data["settings"]["MainMenuCommand"]["CurrentSetting"]):
+        if (insert == dataObject["settings"]["MainMenuCommand"]["CurrentSetting"]):
             return "MainMenu"
 
         if (not insert): return
@@ -49,49 +52,38 @@ def evaluateInput(input):
 
 
 def renderTerminal():
-    if (data["general"]["currentState"] == "startScreen"):
-        print("""
-Hi and welcome to Guess The Number. Please select a section in the menulist, through inputting the given numbers.
-If this is your first time playing this game, I strongly advise you to pay a visit to the settings. Menu: 
-[1]: Settings,
-[2]: StartGame,
+    system("cls")
 
-        """)
+    print(dataObject["text"][dataObject["general"]["currentState"]])
 
-        while (True):
-            insert = playerInput("Please, pick a section", [ValueError], int)
-
-            if (insert == "MainMenu"):
-                data["general"]["currentState"] = "startScreen"
-                return
-
-            if (insert == 1):
-                data["general"]["currentState"] = "Settings"
-                return
-
-            if (insert == 2):
-                return "StartGame"
-        
-
-    elif (data["general"]["currentState"] == "Settings"):
-        print(f"""
-This is the settings, in order to change a setting, please write the setting name (not case sensitive) along with the value. The value outside of the brackets 
-is the current setting and the values inside the brackets are possible settings you can change to. Eg: PlayerMode: Multiplayer.
-for now click on {data["settings"]["MainMenuCommand"]["CurrentSetting"]} to go back to the start screen. 
-
-PlayerMode: {data["settings"]["PlayerMode"]["CurrentSetting"]} (Multiplayer)
-MainMenuCommand: {data["settings"]["MainMenuCommand"]["CurrentSetting"]} (Can be any value. Caution: backwards slash is not allowed, not case sensitive)
-StopGame: {data["settings"]["StopGame"]["CurrentSetting"]} (Can be any value. Caution: backwards slash is not allowed, not case sensitive)
-MinValue: {data["settings"]["MinValue"]["CurrentSetting"]} (Any intiger 0 and above)
-MaxValue: {data["settings"]["MaxValue"]["CurrentSetting"]} (Any intiger 0 and above, if difference between this value and the min value is < 10 this value will be adjusted automatically by the game)
-        """)
-
-        insert = playerInput("Please, pick a section", [ValueError], str)
+    if (dataObject["general"]["currentState"] == "startScreen"):
+        insert = playerInput("Please write a command", [ValueError], int)
 
         if (insert == "MainMenu"):
-            data["general"]["currentState"] = "startScreen"
+            dataObject["general"]["currentState"] = "startScreen"
             return
 
+        if (insert == 1):
+            dataObject["general"]["currentState"] = "Settings"
+            return
+
+        if (insert == 2):
+            dataObject["general"]["currentState"] = "game"
+            return "StartGame"
+        
+    elif (dataObject["general"]["currentState"] == "Settings"):
+        insert = playerInput("Please write a command", [ValueError], int)
+
+        if (insert == "MainMenu"):
+            dataObject["general"]["currentState"] = "startScreen"
+            return
+
+    elif (dataObject["general"]["currentState"] == "game"):
+        insert = playerInput("Please write a command", [ValueError], int)
+
+        if (insert == "MainMenu"):
+            dataObject["general"]["currentState"] = "startScreen"
+            return
 
 
 
---
+++ files/text.py
@@ -0,0 +1,30 @@
+
+
+from data import *
+
+text = {
+    "startScreen": """
+(Caution: The game is not optimized for other operating systems than windows atm)
+Hi and welcome to Guess The Number. Please select a section in the menulist, through inputting the given numbers.
+If this is your first time playing this game, I strongly advise you to pay a visit to the settings. Menu: 
+
+[1]: Settings,
+[2]: StartGame,
+""",
+
+    "Settings": f"""
+This is the settings, in order to change a setting, please write the setting name (not case sensitive) along with the value. The value outside of the brackets 
+is the current setting and the values inside the brackets are possible settings you can change to. Eg: PlayerMode: Multiplayer.
+for now click on {data["settings"]["MainMenuCommand"]["CurrentSetting"]} to go back to the start screen. 
+
+PlayerMode: {data["settings"]["PlayerMode"]["CurrentSetting"]} (Multiplayer)
+MainMenuCommand: {data["settings"]["MainMenuCommand"]["CurrentSetting"]} (Can be any value. Caution: backwards slash is not allowed, not case sensitive)
+StopGame: {data["settings"]["StopGame"]["CurrentSetting"]} (Can be any value. Caution: backwards slash is not allowed, not case sensitive)
+MinValue: {data["settings"]["MinValue"]["CurrentSetting"]} (Any intiger 0 and above)
+MaxValue: {data["settings"]["MaxValue"]["CurrentSetting"]} (Any intiger 0 and above, if difference between this value and the min value is < 10 this value will be adjusted automatically by the game)
+""",
+
+"game": f"""
+Game ongoing, find the hidden number. {data.numEvaluation()}. {data.plrEvaluation}.
+"""
+}
